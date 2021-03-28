7cc63281de1b20607c32aa6db02ebac63d690283
Ted Wollman <25165500+TheTedder@users.noreply.github.com>
2021-03-27 21:34:04 -0400

    Create timer.rs

--- crates/livesplit-auto-splitting/src/lib.rs
+++ crates/livesplit-auto-splitting/src/lib.rs
@@ -3,6 +3,8 @@ mod pointer;
 mod process;
 mod runtime;
 mod std_stream;
+mod timer;
 
 pub use runtime::{Runtime, TimerAction, TimerState};
+pub use timer::Timer;
 pub use wasmtime::InterruptHandle;
