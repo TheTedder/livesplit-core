3b235fb8f086766210016d7972b3d393b620778a
Ted Wollman <25165500+TheTedder@users.noreply.github.com>
2021-04-14 14:03:26 -0400

    Update lib.rs

--- crates/livesplit-auto-splitting/src/lib.rs
+++ crates/livesplit-auto-splitting/src/lib.rs
@@ -4,6 +4,6 @@ mod runtime;
 mod std_stream;
 mod timer;
 
-pub use runtime::{Runtime, TimerAction, TimerState};
-pub use timer::Timer;
+pub use runtime::Runtime;
+pub use timer::{Timer, TimerState};
 pub use wasmtime::InterruptHandle;
